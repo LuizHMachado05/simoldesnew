4be0902823d3720dbbe54244dc33c30c75eee7b9
Luiz Henrique da silva Machado <machadoluiz659@gmail.com>
2025-04-27 00:16:54 -0300

    Add Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/simoldesnew/',  // Deve ser o nome do seu repositório
+  base: '/simoldesnew/',  // IMPORTANTE: deve ser o nome do seu repositório
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
